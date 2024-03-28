e4a1e9c59382239dfc3cc42ad52e2545bf560556
William Kray <github@williamkray.com>
2024-03-28 13:26:07 -0700

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -aP . mssj.me:/home/wreck/docker-matrix/storage/jobmachine/nginx/site/
+rsync -aP . mssj.me:/home/wreck/docker-matrix/storage/jobmachine/nginx/site/ --exclude=".git" --exclude="sync.sh"
