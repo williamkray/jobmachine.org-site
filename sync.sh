f545ab0096d3da495c4dcc0fedc7d703859ac350
William Kray <github@williamkray.com>
2025-02-20 09:10:06 -0800

    fix path in sync script

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -aP . mssj.me:/home/wreck/docker-matrix/storage/jobmachine/nginx/site/ --exclude=".git" --exclude="sync.sh"
+rsync -aP . mssj.me:/home/wreck/jobmachine/site/site --exclude=".git" --exclude="sync.sh"
